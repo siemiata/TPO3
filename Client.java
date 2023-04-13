208771fdbef139dd868636fe1351135f6a7c807a
Aleksander Siemiątkowski <s22201@pjwstk.edu.pl>
2023-04-13 21:32:47 +0200

    update commit

--- Client.java
+++ Client.java
@@ -16,28 +16,19 @@ public class Client {
         int port = 12345; // numer portu
 
         try {
-            // Utworzenie kanału
             channel = SocketChannel.open();
-
-            // Ustalenie trybu nieblokującego
             channel.configureBlocking(false);
-
-            // połączenie kanału
             channel.connect(new InetSocketAddress(server, port));
 
             System.out.print("Klient: łączę się z serwerem ...");
 
             while (!channel.finishConnect()) {
-                // ew. pokazywanie czasu łączenia (np. pasek postępu)
-                // lub wykonywanie jakichś innych (krótkotrwałych) działań
             }
 
         } catch(UnknownHostException exc) {
             System.err.println("Uknown host " + server);
-            // ...
         } catch(Exception exc) {
             exc.printStackTrace();
-            // ...
         }
 
         System.out.println("\nKlient: jestem połączony z serwerem ...");
@@ -45,64 +36,32 @@ public class Client {
         Charset charset  = Charset.forName("ISO-8859-2");
         Scanner scanner = new Scanner(System.in);
 
-        // Alokowanie bufora bajtowego
-        // allocateDirect pozwala na wykorzystanie mechanizmów sprzętowych
-        // do przyspieszenia operacji we/wy
-        // Uwaga: taki bufor powinien być alokowany jednokrotnie
-        // i wielokrotnie wykorzystywany w operacjach we/wy
         int rozmiar_bufora = 1024;
         ByteBuffer inBuf = ByteBuffer.allocateDirect(rozmiar_bufora);
         CharBuffer cbuf = null;
 
-
         System.out.println("Klient: wysyłam - Hi");
-        // "Powitanie" do serwera
         channel.write(charset.encode("Hi\n"));
 
-        // pętla czytania
         while (true) {
+            inBuf.clear();
+            int readBytes = channel.read(inBuf);
 
-            //cbuf = CharBuffer.wrap("coś" + "\n");
-
-            inBuf.clear();	// opróżnienie bufora wejściowego
-            int readBytes = channel.read(inBuf); // czytanie nieblokujące
-            // natychmiast zwraca liczbę
-            // przeczytanych bajtów
-
-            // System.out.println("readBytes =  " + readBytes);
-
-            if (readBytes == 0) {                              // jeszcze nie ma danych
-                //System.out.println("zero bajtów");
-
-                // jakieś (krótkotrwałe) działania np. info o upływającym czasie
-
+            if (readBytes == 0) {
                 continue;
-
             }
-            else if (readBytes == -1) { // kanał zamknięty po stronie serwera
-                // dalsze czytanie niemożlwe
-                // ...
+            else if (readBytes == -1) {
                 break;
             }
-            else {		// dane dostępne w buforze
-                //System.out.println("coś jest od serwera");
-
-                inBuf.flip();	// przestawienie bufora
-
-                // pobranie danych z bufora
-                // ew. decyzje o tym czy mamy komplet danych - wtedy break
-                // czy też mamy jeszcze coś do odebrania z serwera - kontynuacja
+            else {
+                inBuf.flip();
                 cbuf = charset.decode(inBuf);
-
                 String odSerwera = cbuf.toString();
-
                 System.out.println("Klient: serwer właśnie odpisał ... " + odSerwera);
                 cbuf.clear();
 
                 if (odSerwera.equals("Bye")) break;
             }
-
-            // Teraz klient pisze do serwera poprzez Scanner
             String input = scanner.nextLine();
             cbuf = CharBuffer.wrap(input + "\n");
             ByteBuffer outBuf = charset.encode(cbuf);
@@ -110,8 +69,6 @@ public class Client {
 
             System.out.println("Klient: piszę " + input);
         }
-
         scanner.close();
-
     }
 }
